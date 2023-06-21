a303280421c07006dae8118dfefc7dfa5ebd0b21
YasasDaraka <darakayasas@gmail.com>
2023-06-21 11:26:54 +0530

    Create classes

---
+++ ChatApplication/Client1/src/main/java/controller/Client1Controller.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class Client1Controller {
+    public void onActionImageSend(MouseEvent mouseEvent) {
+    }
+
+    public void onActionSendMassage(MouseEvent mouseEvent) {
+    }
+}
