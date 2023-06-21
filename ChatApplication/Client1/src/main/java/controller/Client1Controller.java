ea2a29579ed890026cb3abab7b8d6d253c418c03
YasasDaraka <darakayasas@gmail.com>
2023-06-21 18:44:57 +0530

    modify all

--- ChatApplication/Client1/src/main/java/controller/Client1Controller.java
+++ ChatApplication/Client1/src/main/java/controller/Client1Controller.java
@@ -216,4 +216,52 @@ public class Client1Controller {
         txtMassage.clear ();
 
     }
+
+    public void sad(MouseEvent mouseEvent) {
+    }
+
+    public void lot_sad(MouseEvent mouseEvent) {
+    }
+
+    public void money(MouseEvent mouseEvent) {
+    }
+
+    public void love(MouseEvent mouseEvent) {
+    }
+
+    public void green_sad(MouseEvent mouseEvent) {
+    }
+
+    public void smile_one_eyy(MouseEvent mouseEvent) {
+    }
+
+    public void cry_yes(MouseEvent mouseEvent) {
+    }
+
+    public void cry_head(MouseEvent mouseEvent) {
+    }
+
+    public void real_amile(MouseEvent mouseEvent) {
+    }
+
+    public void tuin(MouseEvent mouseEvent) {
+    }
+
+    public void woow(MouseEvent mouseEvent) {
+    }
+
+    public void smile_normal(MouseEvent mouseEvent) {
+    }
+
+    public void large_smile(MouseEvent mouseEvent) {
+    }
+
+    public void small_smile(MouseEvent mouseEvent) {
+    }
+
+    public void tong_smile(MouseEvent mouseEvent) {
+    }
+
+    public void emo_pane_on_action(MouseEvent mouseEvent) {
+    }
 }
